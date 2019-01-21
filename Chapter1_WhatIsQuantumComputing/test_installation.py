1a854e373fad68712598b940846f7a93f8d1bcaa
Christine Corbett <christine.corbett@gmail.com>
2019-01-21 10:20:59 -0800

    updated script to test installation

--- Chapter1_WhatIsQuantumComputing/test_installation.py
+++ Chapter1_WhatIsQuantumComputing/test_installation.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
-
+import sys
 # The following code tests your Python installation
 
 try:
 	import numpy
 	import matplotlib
 except:
-	raise "make sure to pip install -r requirements.txt"
+	raise Exception("make sure to pip install -r requirements.txt")
 
-try:
-	print ""
-	raise "You are running Python 2. Python 3.4+ is required for code examples"
-except:
-	pass
+
+
+if not (sys.version_info > (3, 0)):
+	raise Exception("You are running Python 2. Python 3.4+ is required for code examples")
+
